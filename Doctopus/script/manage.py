a50bca59bde8db7557b1f43197674a9dd37d7391
piaoliu <441594700@qq.com>
2017-08-16 15:08:01 +0800

    Http API preliminary identified, divided into client and server version

--- Doctopus/script/manage.py
+++ Doctopus/script/manage.py
@@ -5,8 +5,16 @@ import argparse
 from Doctopus.web.app import get_app
 
 if __name__ == '__main__':
-    parse = argparse.ArgumentParser(description='A distributed data collector.')
-    parse.add_argument('action', action='store')
+    parse = argparse.ArgumentParser(prog='Doctopus', description='A distributed data collector.')
+    parse.add_argument('-a', '--action', action='store', default='run', help='Run the project')
+    parse.add_argument('-v', '--version', action='version', default=None, version='%(prog)s 0.1.0')
+    parse.add_argument('-t', '--target', default='client', choices=['server', 'client'], help='default run client')
     command = parse.parse_args().action
+    target = parse.parse_args().target
+    version = parse.parse_args().version
     if command == 'run':
-        waitress.serve(get_app(), host='0.0.0.0', port=8000)
+        waitress.serve(get_app(target), host='0.0.0.0', port=8000)
+    elif command == 'test':
+        pass
+    elif version:
+        print(version)
--- Doctopus/web/app.py
+++ Doctopus/web/app.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 import falcon
-import waitress
-from Doctopus.web.data import Status
+
+from Doctopus.web.data import Status, Reload, Restart, SeverStatus, NodeStatus
 
 from Doctopus.utils.util import get_conf
 
 conf = get_conf('conf/conf.toml')
 
 
-def create_app(conf):
+def create_client(conf):
     api = falcon.API()
     api.add_route('/status/', Status(conf))
+    api.add_route('/reload/', Reload(conf))
+    api.add_route('/restart/', Restart(conf))
     return api
 
-def get_app():
-    return create_app(conf)
+
+def create_server(conf):
+    api = falcon.API()
+    api.add_route('/status/', SeverStatus(conf))
+    api.add_route('/status/{node}/', NodeStatus(conf))
+    return api
 
 
-
+def get_app(selection):
+    if selection == "client":
+        return create_client(conf)
+    elif selection == "server":
+        return create_server(conf)

7dc8a1e0639fc535bab26702db39c342dd9f0d34
piaoliu <441594700@qq.com>
2017-08-23 16:22:31 +0800

    增加 logging 模块的使用，替换掉 logbook： - Loggers expose the interface that application code directly uses. - Handlers send the log records (created by loggers) to the appropriate destination. - Filters provide a finer grained facility for determining which log records to output. - Formatters specify the layout of log records in the final output.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(exclude=[]),
     include_package_data=True,
     license='MIT',
-    install_requires=['pendulum', 'logbook', 'redis',
+    install_requires=['pendulum', 'redis',
                       'influxdb', 'msgpack-python', 'toml', 'u-msgpack-python', 'falcon'],
     entry_points={
         'console_scripts': [
